c523ec306a1a36d1ac56783883df2fbabc3d7556
Trong Tai <tai.nguyen7922@gmail.com>
2024-04-03 13:37:10 +0700

    first commit

---
+++ src/main/java/com/service/service/ChutiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChutiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChutiServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/service/service/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.service.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String index(){
+        return "Hello World!";
+    }
+}
