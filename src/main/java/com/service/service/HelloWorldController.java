38719038375ba2b0ef8030f9230dd17fe44bb491
Trong Tai <tai.nguyen7922@gmail.com>
2024-05-05 12:23:07 +0700

    demo merge

--- src/main/java/com/service/service/HelloWorldController.java
+++ src/main/java/com/service/service/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/")
     public String index(){
-        return "Hello World! 123";
+        return "Hello World! 123 demo merge";
     }
 }
